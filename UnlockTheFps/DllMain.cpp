fd8c2dea2b686039f8a819b6d7141f5e0147691b
techiew <marius.tw@hotmail.no>
2022-03-25 21:13:52 +0100

    Added configurable FPS limit

--- UnlockTheFps/DllMain.cpp
+++ UnlockTheFps/DllMain.cpp
@@ -1,15 +1,43 @@
 #include <Windows.h>
+#include <algorithm>
 
 #include "ModUtils.h"
 
 using namespace ModUtils;
+using namespace mINI;
+
+static float fpsLimit = 999;
+
+void ReadConfig()
+{
+	INIFile config(GetModuleFolderPath() + "\\config.ini");
+	INIStructure ini;
+
+	if (config.read(ini))
+	{
+		fpsLimit = std::stof(ini["unlockthefps"].get("limit"));
+	}
+	else
+	{
+		ini["unlockthefps"]["limit"] = "999";
+		config.write(ini, true);
+	}
+
+	Log("FPS limit: %f", fpsLimit);
+}
 
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
 	Log("Activating UnlockTheFps...");
 	std::vector<uint16_t> pattern = { 0xc7, 0x43, 0x20, 0x89, 0x88, 0x88, 0x3c, 0xeb, 0x43, 0x89, 0x73, 0x18, 0xeb, 0xca, 0x89, 0x73, 0x18 };
 	std::vector<uint16_t> originalBytes = { 0x89, 0x88, 0x88, 0x3c };
-	std::vector<uint8_t> newBytes = { 0x3a, 0x83, 0x34, 0x05 };
+	std::vector<uint8_t> newBytes(4, 0x90);
+
+	ReadConfig();
+	float frametime = (1000 / fpsLimit) / 1000;
+	Log("Frametime: %f", frametime);
+	memcpy(&newBytes[0], &frametime, 4);
+
 	uintptr_t patchAddress = SigScan(pattern);
 	if (patchAddress == 0)
 	{
